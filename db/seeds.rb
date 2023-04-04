bea23ad5eb716a2124dcd0c9b19dfffae6e7050a
reaganongaya <reagan.ongaya@student.moringaschool.com>
2023-04-04 10:55:03 +0300

    Assignment

---
+++ db/seeds.rb
@@ -0,0 +1,35 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+Airline.create([
+    {
+        name: "Kenya Airways"
+        image_url: "https://pbs.twimg.com/profile_images/1105088380470206464/K5UEUfFg_400x400.jpg"
+    },
+    {
+        name: "Ethiopian Airlines"
+        image_url:"https://download.logo.wine/logo/Ethiopian_Airlines/Ethiopian_Airlines-Logo.wine.png"
+    },
+    {
+        name: "South African Airline"
+        image_url: "https://cdn.freebiesupply.com/logos/large/2x/south-african-airways-2-logo-png-transparent.png"
+    },
+    {
+        name: "Emirates Fly"
+        image_url:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Emirates_logo.svg/850px-Emirates_logo.svg.png"
+    },
+    {
+        name: "Paris Airlines"
+        image_url: "https://logos-world.net/wp-content/uploads/2020/03/Air-France-Logo-2009-2016.jpg"
+    },
+    {
+        name: "Jetex"
+        name: "https://is3-ssl.mzstatic.com/image/thumb/Purple122/v4/80/ca/b5/80cab545-65bb-f525-3729-3c5c5f9c61e8/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/1200x630wa.png"
+    },
+])
